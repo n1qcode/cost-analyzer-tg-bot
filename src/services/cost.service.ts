e6e8dc46181ed60946772594b8b5d71815fa8f36
Nick <n1kvd@yandex.ru>
2023-05-21 20:54:44 +0300

    fix: removed deprecated api requests

--- src/services/cost.service.ts
+++ src/services/cost.service.ts
@@ -7,24 +7,12 @@ export const costService = {
     user_id: number;
     cost_category: string;
     cost_amount: number;
-  }) => $api.put("/user/cost/amount", props),
-  getDayCostOfUser: async (user_id: number, cost_date: string) =>
-    $api(`/user/${user_id}/cost/day/${cost_date}`),
+  }) => $api.put("/cost/amount", props),
   getDayCost: async (cost_date: string) => $api.get(`/cost/day/${cost_date}`),
-  getAllCostOfUser: async (props: { cost_category: string }) =>
-    console.log(props),
   getAllCost: async (props: { cost_category: string }) => console.log(props),
-  getYearCostOfUser: async (props: { cost_category: string }) =>
-    console.log(props),
   getYearCost: async (props: { cost_category: string }) => console.log(props),
-  getSeasonCostOfUser: async (props: { cost_category: string }) =>
-    console.log(props),
   getSeasonCost: async (props: { cost_category: string }) => console.log(props),
-  getMonthCostOfUser: async (props: { cost_category: string }) =>
-    console.log(props),
   getMonthCost: async (props: { cost_category: string }) => console.log(props),
-  getPeriodCostOfUser: async (props: { cost_category: string }) =>
-    console.log(props),
   getPeriodCost: async (props: { cost_category: string }) => console.log(props),
   getCostCategories: async () => $api("/cost/categories"),
 };

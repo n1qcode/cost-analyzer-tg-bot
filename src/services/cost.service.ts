006510504a43bf2cf1776fe5d1b7fff1015df991
Nick <n1kvd@yandex.ru>
2023-05-23 10:38:48 +0300

    fix: fixed requests methods of cost service

--- src/services/cost.service.ts
+++ src/services/cost.service.ts
@@ -2,18 +2,18 @@ import $api from "../http";
 
 export const costService = {
   createCostCategory: (props: { cost_category: string }) =>
-    $api.put("/cost", props),
+    $api.post("/cost", props),
   addToCostCategory: async (props: {
     user_id: number;
     cost_category: string;
     cost_amount: number;
   }) => $api.put("/cost/amount", props),
+  getCostCategories: async () => $api.get("/cost/categories"),
   getDayCost: async (cost_date: string) => $api.get(`/cost/day/${cost_date}`),
+  getMonthCost: async (year: string, month: string) =>
+    $api.get(`/cost/year/${year}/month/${month}`),
   getAllCost: async (props: { cost_category: string }) => console.log(props),
   getYearCost: async (props: { cost_category: string }) => console.log(props),
   getSeasonCost: async (props: { cost_category: string }) => console.log(props),
-  getMonthCost: async (year: string, month: string) =>
-    $api.get(`/cost/year/${year}/month/${month}`),
   getPeriodCost: async (props: { cost_category: string }) => console.log(props),
-  getCostCategories: async () => $api("/cost/categories"),
 };

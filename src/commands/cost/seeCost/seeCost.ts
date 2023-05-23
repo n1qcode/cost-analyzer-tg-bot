9bf4c5dc2631d45b2c42fdc95c8960421e949241
Nick <n1kvd@yandex.ru>
2023-05-23 16:41:19 +0300

    fix: inactive unready buttons

--- src/commands/cost/seeCost/seeCost.ts
+++ src/commands/cost/seeCost/seeCost.ts
@@ -14,10 +14,10 @@ const seeCost = (bot: Telegraf<IBotContext>) => {
       ...Markup.inlineKeyboard([
         [Markup.button.callback(t("today_cost"), "today_cost")],
         [Markup.button.callback(t("month_cost"), "month_cost")],
-        [Markup.button.callback(t("season_cost"), "season_cost")],
-        [Markup.button.callback(t("year_cost"), "year_cost")],
-        [Markup.button.callback(t("choose_date_cost"), "choose_date_cost")],
-        [Markup.button.callback(t("choose_period_cost"), "choose_period_cost")],
+        // [Markup.button.callback(t("season_cost"), "season_cost")], // TODO
+        // [Markup.button.callback(t("year_cost"), "year_cost")], // TODO
+        // [Markup.button.callback(t("choose_date_cost"), "choose_date_cost")], // TODO
+        // [Markup.button.callback(t("choose_period_cost"), "choose_period_cost")], // TODO
       ]),
     });
   });
